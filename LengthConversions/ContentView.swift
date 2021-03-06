7d9b6bd67d1ee812e611fd4bab4a3b179643eacc
Nikola Anastasovski <nikola.anastasovski@netcetera.com>
2021-03-06 19:52:53 +0100

    Created LengthConversion app, SwiftUI 100 day Challenge day 19.

--- LengthConversions/ContentView.swift
+++ LengthConversions/ContentView.swift
@@ -8,9 +8,64 @@
 import SwiftUI
 
 struct ContentView: View {
+  
+    let units = ["meter", "kilometer", "feet", "yards", "miles"]
+    
+    @State private var unitFromId = 0
+    @State private var unitToId = 1
+    @State private var lengthText = ""
+    
+    var unitFrom: String {
+        units[unitFromId]
+    }
+    
+    var unitTo: String {
+        units[unitToId]
+    }
+    
+    let amountInMeterOf: [String: Double] = [
+        "meter": 1,
+        "kilometer": 1000,
+        "feet": 0.3048,
+        "yards": 0.9144,
+        "miles": 1609.34
+    ]
+    
+    var result: Double {
+        let length = Double(lengthText) ?? 0
+        return length * amountInMeterOf[unitFrom]! / amountInMeterOf[unitTo]!
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Length", text: $lengthText)
+                }
+                
+                Section(header: Text("From")) {
+                    Picker("From", selection: $unitFromId) {
+                        ForEach(0..<units.count) {
+                            Text("\(units[$0])")
+                        }
+                    }.pickerStyle(SegmentedPickerStyle())
+                }
+                
+                Section(header: Text("To")) {
+                    Picker("To", selection: $unitToId) {
+                        ForEach(0..<units.count) {
+                            Text("\(units[$0])")
+                        }
+                    }.pickerStyle(SegmentedPickerStyle())
+                }
+                
+                Section(header: Text("Result")) {
+                    Text(String(format: "%.2f", result))
+                }
+            }
+            .navigationTitle("Length Convertor")
+            .navigationBarTitleDisplayMode(.inline)
+        }
     }
 }
 
