5373328e69d9c560605a39eb93d28e6283668b1d
Nikola Anastasovski <nikola.anastasovski@netcetera.com>
2021-03-06 12:15:54 +0100

    Initial Commit

---
+++ LengthConversions/LengthConversionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LengthConversionsApp.swift
+//  LengthConversions
+//
+//  Created by Nikola Anastasovski on 6.3.21.
+//
+
+import SwiftUI
+
+@main
+struct LengthConversionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
